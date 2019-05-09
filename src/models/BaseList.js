cb541ad4e7bd371657635350fb610994e7c83f1e
Holly Gibson <hollyglotapps@gmail.com>
2019-05-09 16:21:12 +0300

    Add base and baselist models

---
+++ src/models/BaseList.js
@@ -0,0 +1,37 @@
+import { Record, List } from 'immutable'
+
+const defaultValues = {
+  loading: false,
+  errorMessage: '',
+  data: new List(),
+}
+
+export default class BaseList extends Record(defaultValues, BaseList) {
+  constructor(values) {
+    super({
+      ...values,
+    })
+  }
+
+  static findByKeyValue(items, key, value) {
+    return items.find((item) => {
+      return item.get(`${ key }`) === value ? item : undefined
+    })
+  }
+
+  static findIndexByKeyValue(items, key, value) {
+    return items.findIndex((item) => {
+      return item.get(`${ key }`) === value ? item : undefined
+    })
+  }
+}
+
+export const toEntityList = (data, Entity) => {
+  let entityItems = new List()
+  if (data) {
+    data.forEach((value) => {
+      entityItems = entityItems.push(new Entity(value))
+    })
+  }
+  return entityItems
+}
