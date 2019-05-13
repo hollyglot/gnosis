92f9a5e1d1ca7269266a46a8b4b8019bce70fa5a
Holly Gibson <hollyglotapps@gmail.com>
2019-05-14 02:37:21 +0300

    Add options to BaseList

--- src/models/BaseList.js
+++ src/models/BaseList.js
@@ -1,8 +1,9 @@
-import { Record, List } from 'immutable';
+import { Record, List, Map } from 'immutable';
 
 const defaultValues = {
   loading: false,
   errorMessage: '',
+  options: new Map(),
   data: new List(),
 };
 
@@ -10,6 +11,7 @@ export default class BaseList extends Record(defaultValues, 'BaseList') {
   constructor(values) {
     super({
       ...values,
+      options: values && values.options ? values.options : defaultValues.options,
     });
   }
 }
