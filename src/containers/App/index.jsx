b1f0083b90ad5ea4ef860d002ccd57ef8c855b4f
Holly Gibson <hollyglotapps@gmail.com>
2019-05-14 01:54:26 +0300

    Setup muitheme

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
 import './styles.css';
 import Quiz from '../Quiz';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import teal from '@material-ui/core/colors/teal';
+import amber from '@material-ui/core/colors/amber';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: teal,
+    secondary: amber,
+  },
+  typography: {
+    fontFamily: [
+      'Nanum Gothic',
+      'serif',
+    ].join(','),
+  },
+});
 
 class App extends Component {
   render() {
     return (
       <div className="application">
-        <header className="application-header">
-          {/*
-            TODO: Add logo for gnosis
-          <img className="application-logo" alt="logo" />
-          }*/}
-          <h1>
-            Test your knowledge
-          </h1>
-        </header>
         <Quiz />
+        <MuiThemeProvider theme={ theme }>
+          <CssBaseline />
+          <AppBar position="static" color="primary">
+            <Toolbar>
+              <Typography variant="h2" color="inherit">
+                γνῶσις
+                <Typography variant="subtitle1" color="inherit">
+                  <span className='italicize'>gnōsis</span> | knowledge
+                </Typography>
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </MuiThemeProvider>
       </div>
     );
   }
