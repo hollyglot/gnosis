edfb14be2e2dfd320110720e45ad76767d5433c9
Holly Gibson <hollyglotapps@gmail.com>
2019-05-13 18:41:39 +0300

    Change App container to class

---
+++ src/containers/App/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="application">
+        <header className="application-header">
+          {/*
+            TODO: Add logo for gnosis
+          <img className="application-logo" alt="logo" />
+          }*/}
+          <h1>
+            Test your knowledge
+          </h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
