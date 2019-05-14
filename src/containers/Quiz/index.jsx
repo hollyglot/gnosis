85bed298e3d2290d20c27db819e00f0e05023770
Holly Gibson <hollyglotapps@gmail.com>
2019-05-14 04:34:58 +0300

    Add question options to Quiz and render TextArea

--- src/containers/Quiz/index.jsx
+++ src/containers/Quiz/index.jsx
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import './styles.css';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-import { getQuestions } from '../../actions/questions';
+import { getQuestions, setCurrentQuestion } from '../../actions/questions';
 import LoadingIndicator from '../../components/LoadingIndicator';
+import TextArea from '../../components/TextArea';
+import { QUESTION_KEYS } from '../../models/Question';
 
 const mapStateToProps = state => {
   return {
     questions: state.questions.get('data'),
+    options: state.questions.get('options'),
     loading: state.questions.get('loading'),
   };
 };
@@ -24,6 +28,7 @@ const mapDispatchToProps = dispatch => {
 export class Quiz extends Component {
   static propTypes = {
     questions: PropTypes.object.isRequired,
+    options: PropTypes.object.isRequired,
     loading: PropTypes.bool.isRequired,
     dispatch: PropTypes.func.isRequired,
   }
@@ -38,34 +43,45 @@ export class Quiz extends Component {
   }
 
   renderQuestions() {
-    const { questions } = this.props;
+    const { questions, options } = this.props;
 
     if (questions.size === 0) {
       return "Unable to load questions at this time.";
     }
 
-    return (
-      <ul className='questions list'>
-        { questions.map((question) => {
-            return (<li>{ question.question }</li>);
-          })
-        }
-      </ul>
-    );
+    const currentPosition = options.get(QUESTION_KEYS.CURRENT_QUESTION);
+    const currentQuestion = questions.get(currentPosition);
+
+    if (currentQuestion.choices.size) {
+      return (<Typography variant="h6" color="inherit">
+        so many choices...
+      </Typography>);
+    }
+
+    return (<TextArea question={ currentQuestion } />);
   }
 
   render() {
     const { loading } = this.props;
     return (
-      <div className="quiz">
-        <Typography variant="h4" color="inherit">
-          Test your knowledge
-        </Typography>
-        <hr />
-        { loading ? this.renderLoader()
-          : this.renderQuestions()
-        }
-      </div>
+      <Grid container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        spacing={ 24 }
+      >
+        <Grid item xs={ 12 } md={ 12 } lg={ 12 } style={{ paddingTop: 48 }}>
+          <Typography variant="h4" color="inherit">
+            Test your knowledge
+          </Typography>
+          <hr />
+        </Grid>
+        <Grid item xs={ 12 } md={ 12 } lg={ 12 }>
+          { loading ? this.renderLoader()
+            : this.renderQuestions()
+          }
+        </Grid>
+      </Grid>
     );
   }
 }
